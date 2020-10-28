a3f5320f61294afc17fe3d80c6f24aea9b7f1ace
leny62 <lihirwe6@gmail.com>
2020-10-28 16:14:14 +0200

    no edit

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import './src/models/db';
-import { set, connect } from 'mongoose';
-set('useNewUrlParser', true);
-set('useUnifiedTopology', true);
-
+import { set } from 'mongoose';
 import express from 'express';
 import { json, urlencoded } from 'body-parser';
 import dotenv from 'dotenv';
 import usersRouter from './src/routes/users';
 import blogsRoutes from './src/routes/blogs';
 import inquiryRoutes from './src/routes/inquiries';
+
+
+set('useNewUrlParser', true);
+set('useUnifiedTopology', true);
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use(`${process.env.API_VERSION}/blogs`,blogsRoutes);
 app.use(`${process.env.API_VERSION}/inquiries`,inquiryRoutes);
 
 const port = process.env.PORT;
+
 app.listen(port,()=>{ 
     console.log("App connected "+port);
 })
